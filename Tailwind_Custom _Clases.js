4ed14bd6f00e79ae704ba8381056669cbc5185c9
EminatarenX <115912751+EminatarenX@users.noreply.github.com>
2023-06-10 20:05:03 -0600

    Create Tailwind_Custom _Clases.js
    custom clases para tailwind css

---
+++ Tailwind_Custom _Clases.js
@@ -0,0 +1,19 @@
+extend: {
+      keyframes: {
+        'entrada':{
+          '0%':{
+            opacity: '0',
+
+          },
+          '100%': {
+            opacity: '1'
+          }
+        }
+      },
+      animation: {
+        'entrada': 'entrada 2s'
+      },
+       colors: {
+         'rojo': '#roojo'
+       }
+    },
