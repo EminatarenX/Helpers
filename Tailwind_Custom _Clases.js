6e29b1d2f34a4c1cc7a2eb4a8a4b1916d1d3b396
EminatarenX <115912751+EminatarenX@users.noreply.github.com>
2023-06-12 18:35:06 -0600

    Update Tailwind_Custom _Clases.js
    i add more animations and colors of preference

--- Tailwind_Custom _Clases.js
+++ Tailwind_Custom _Clases.js
@@ -8,12 +8,26 @@ extend: {
           '100%': {
             opacity: '1'
           }
+        },
+        'slide': {
+          '0%': {
+            transform: 'traslateX(-400px)'
+          }
+          '100%': {
+            transform: 'traslateX(0px)
+          }
         }
       },
       animation: {
         'entrada': 'entrada 2s'
       },
-       colors: {
-         'rojo': '#roojo'
-       }
+      colors: {
+         'rojo': '#d41935',
+         'azul': '#2780c4'.
+         'a-oscuro': '#041833',
+       },
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(to right, #ff00ff, #00ffff)',
+      }, 
+            
     },
